e0bec1d28928a90137b7d22694aab563002af94e
lausek <root@lausek.eu>
2020-11-29 10:23:40 +0100

    add library and specs

---
+++ 03_bibliothek/builder/src/Application.java
@@ -0,0 +1,11 @@
+public class Application {
+    public static void main(String... args) {
+        Pizza pizza = new Pizza.Builder(12)
+                .cheese(true)
+                .pepperoni(true)
+                .bacon(true)
+                .build();
+
+        System.out.println(pizza);
+    }
+}
---
+++ 03_bibliothek/builder/src/Pizza.java
@@ -0,0 +1,51 @@
+public class Pizza {
+    private int size;
+    private boolean cheese;
+    private boolean pepperoni;
+    private boolean bacon;
+
+    private Pizza(Builder builder) {
+        size = builder.size;
+        cheese = builder.cheese;
+        pepperoni = builder.pepperoni;
+        bacon = builder.bacon;
+    }
+
+    public String toString() {
+        return "{ Pizza : size = " + size + " - cheese = " + cheese +
+                " - pepperoni = " + pepperoni + " - bacon = " + bacon + " }";
+    }
+
+    public static class Builder {
+        //required
+        private final int size;
+
+        //optional
+        private boolean cheese = false;
+        private boolean pepperoni = false;
+        private boolean bacon = false;
+
+        public Builder(int size) {
+            this.size = size;
+        }
+
+        public Builder cheese(boolean value) {
+            cheese = value;
+            return this;
+        }
+
+        public Builder pepperoni(boolean value) {
+            pepperoni = value;
+            return this;
+        }
+
+        public Builder bacon(boolean value) {
+            bacon = value;
+            return this;
+        }
+
+        public Pizza build() {
+            return new Pizza(this);
+        }
+    }
+}
